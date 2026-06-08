e51d704517b05f600de3f96ee4dfd50867472c7f
shirley-0110 <shirley.ho@qps-taiwan.com>
2026-06-08 15:06:46 +0800

    Update app.py

--- app.py
+++ app.py
@@ -3130,11 +3130,17 @@ def make_step1_cache_key(file_bytes):
 # =================================================================================================================
 def fetch_url(url):
     headers = {
-        "User-Agent": "Mozilla/5.0"
+        "User-Agent": "Mozilla/5.0",
+        "Accept": "text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8",
+        "Accept-Language": "en-US,en;q=0.9"
     }
     resp = requests.get(url, headers=headers, timeout=30)
     resp.raise_for_status()
     return resp
+
+
+
+
 
 
 def get_latest_archive_txt():
